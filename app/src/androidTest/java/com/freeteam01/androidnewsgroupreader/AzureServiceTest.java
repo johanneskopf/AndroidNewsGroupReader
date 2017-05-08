485eb617707464acbd22bf69c23a79837e220e19
Johannes Kopf <johannes.kopf@student.tugraz.at>
2017-05-08 18:39:27 +0200

    [MZ,JK] extended AzureService test cases

--- app/src/androidTest/java/com/freeteam01/androidnewsgroupreader/AzureServiceTest.java
+++ app/src/androidTest/java/com/freeteam01/androidnewsgroupreader/AzureServiceTest.java
@@ -6,6 +6,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import com.freeteam01.androidnewsgroupreader.Models.NewsGroupArticle;
+import com.freeteam01.androidnewsgroupreader.Models.NewsGroupEntry;
 import com.freeteam01.androidnewsgroupreader.Models.ToDoItem;
 import com.freeteam01.androidnewsgroupreader.Services.AzureService;
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
@@ -24,20 +25,19 @@ import static junit.framework.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class AzureServiceTest {
     @Before
-    public void init()
-    {
+    public void init() {
         Context appContext = InstrumentationRegistry.getTargetContext();
         assertEquals("com.freeteam01.androidnewsgroupreader", appContext.getPackageName());
-        if(!AzureService.isInitialized())
+        if (!AzureService.isInitialized())
             AzureService.Initialize(appContext);
     }
 
     @Test
     public void insertData_isCorrect() throws Exception {
-        MobileServiceClient mClient = AzureService.getInstance().getClient();
-        MobileServiceTable<ToDoItem> mToDoTable = mClient.getTable(ToDoItem.class);
+        MobileServiceClient client = AzureService.getInstance().getClient();
+        MobileServiceTable<ToDoItem> toDoTable = client.getTable(ToDoItem.class);
 
-        int itemsCountBefore = mToDoTable.execute().get().size();
+        int itemsCountBefore = toDoTable.execute().get().size();
         int itemsToInsert = 5;
         List<ToDoItem> insertedItems = new ArrayList<>();
 
@@ -45,47 +45,82 @@ public class AzureServiceTest {
             ToDoItem item = new ToDoItem();
             item.setText("TestItem_" + (i + 1));
             item.setComplete(false);
-            ToDoItem entity = mToDoTable.insert(item).get();
+            ToDoItem entity = toDoTable.insert(item).get();
             insertedItems.add(entity);
         }
 
         assertEquals(itemsToInsert, insertedItems.size());
 
         for (ToDoItem item : insertedItems) {
-            mToDoTable.delete(item);
+            toDoTable.delete(item);
         }
 
-        int itemsCountAfter = mToDoTable.execute().get().size();
+        int waitTillDeletionCompleted = 1000;
+        Thread.sleep(waitTillDeletionCompleted);
+
+        int itemsCountAfter = toDoTable.execute().get().size();
         assertEquals(itemsCountBefore, itemsCountAfter);
     }
 
     @Test
     public void insertNewsGroupArticle_isCorrect() throws Exception {
         Log.d(TAG, "Started testing");
-        MobileServiceClient mClient = AzureService.getInstance().getClient();
+        MobileServiceClient client = AzureService.getInstance().getClient();
 
-        MobileServiceTable<NewsGroupArticle> mToDoTable = mClient.getTable(NewsGroupArticle.class);
+        MobileServiceTable<NewsGroupArticle> articleTable = client.getTable(NewsGroupArticle.class);
 
-        int itemsCountBefore = mToDoTable.execute().get().size();
+        int itemsCountBefore = articleTable.execute().get().size();
         int itemsToInsert = 5;
         List<NewsGroupArticle> insertedItems = new ArrayList<>();
 
         for (int i = 0; i < itemsToInsert; i++) {
             NewsGroupArticle item = new NewsGroupArticle("" + (i + 1), "Math", "01.01.2017", "MaxMustermann");
-            NewsGroupArticle entity = mToDoTable.insert(item).get();
+            NewsGroupArticle entity = articleTable.insert(item).get();
             insertedItems.add(entity);
         }
 
         assertEquals(itemsToInsert, insertedItems.size());
 
         for (NewsGroupArticle item : insertedItems) {
-            mToDoTable.delete(item);
+            articleTable.delete(item);
         }
 
         int waitTillDeletionCompleted = 1000;
         Thread.sleep(waitTillDeletionCompleted);
 
-        int itemsCountAfter = mToDoTable.execute().get().size();
+        int itemsCountAfter = articleTable.execute().get().size();
+        assertEquals(itemsCountBefore, itemsCountAfter);
+
+        Log.d(TAG, "Finished testing");
+    }
+
+    @Test
+    public void insertNewsGroupEntry_isCorrect() throws Exception {
+        Log.d(TAG, "Started testing");
+        MobileServiceClient client = AzureService.getInstance().getClient();
+
+        MobileServiceTable<NewsGroupEntry> entryTable = client.getTable(NewsGroupEntry.class);
+
+        int itemsCountBefore = entryTable.execute().get().size();
+        int itemsToInsert = 5;
+        List<NewsGroupEntry> insertedItems = new ArrayList<>();
+
+        for (int i = 0; i < itemsToInsert; i++) {
+            NewsGroupEntry item = new NewsGroupEntry(i, "Entry_" + (i + 1), false);
+            NewsGroupEntry entity = entryTable.insert(item).get();
+            insertedItems.add(entity);
+        }
+
+        assertEquals(itemsToInsert, insertedItems.size());
+
+        for (NewsGroupEntry item : insertedItems) {
+            entryTable.delete(item);
+        }
+
+        int waitTillDeletionCompleted = 1000;
+        Thread.sleep(waitTillDeletionCompleted);
+
+        int itemsCountAfter = entryTable.execute().get().size();
         assertEquals(itemsCountBefore, itemsCountAfter);
 
         Log.d(TAG, "Finished testing");
