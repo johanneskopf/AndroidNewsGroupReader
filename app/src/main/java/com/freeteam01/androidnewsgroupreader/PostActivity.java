239ccf1e9ef1ba6f840843220f66631b2c85c020
Lukas Raab <lukas.raab@student.tugraz.at>
2017-05-08 16:37:31 +0200

    [CP, LR] UI Design of Answer Form

--- app/src/main/java/com/freeteam01/androidnewsgroupreader/PostActivity.java
+++ app/src/main/java/com/freeteam01/androidnewsgroupreader/PostActivity.java
@@ -1,17 +1,24 @@
 package com.freeteam01.androidnewsgroupreader;
 
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.text.method.ScrollingMovementMethod;
+import android.text.style.CharacterStyle;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.freeteam01.androidnewsgroupreader.Models.NewsGroupArticle;
 import com.freeteam01.androidnewsgroupreader.Services.NewsGroupService;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class PostActivity extends AppCompatActivity {
@@ -19,18 +26,26 @@ public class PostActivity extends AppCompatActivity {
     private static final int REQUEST_INTERNET = 0;
     private TextView postTextView_;
     private String article_id_;
+    private EditText et_answer_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_activity);
 
-        article_id_ = (String)getIntent().getExtras().get("article");
+        article_id_ = (String) getIntent().getExtras().get("article");
         postTextView_ = (TextView) findViewById(R.id.postText);
+        et_answer_ = (EditText) findViewById(R.id.et_answer);
 
         LoadNewsGroupsArticles loader = new LoadNewsGroupsArticles();
         loader.execute();
 
+        postTextView_.setMovementMethod(new ScrollingMovementMethod());
+
+
+        postTextView_.setCustomSelectionActionModeCallback(new StyleCallback());
+
+        et_answer_.setCustomSelectionActionModeCallback(new StyleCallback());
         permissionCheck();
     }
 
@@ -59,7 +74,7 @@ public class PostActivity extends AppCompatActivity {
                 article_text.add(service.getArticleText(article_id_));
                 service.Disconnect();
             } catch (Exception e) {
-                Log.e("LOAD_TEXT",Log.getStackTraceString(e));
+                Log.e("LOAD_TEXT", Log.getStackTraceString(e));
             }
             return article_text;
         }
@@ -69,4 +84,68 @@ public class PostActivity extends AppCompatActivity {
         }
     }
 
+    /*
+    *
+    * adapted from http://stackoverflow.com/questions/12995439/custom-cut-copy-action-bar-for-edittext-that-shows-text-selection-handles
+    *
+    */
+
+    private class StyleCallback implements android.view.ActionMode.Callback {
+
+        @Override
+        public boolean onCreateActionMode(android.view.ActionMode mode, Menu menu) {
+            Log.d("FORMAT", "onCreateActionMode");
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.menu_selection, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(android.view.ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(android.view.ActionMode mode, MenuItem item) {
+            Log.d("FORMAT", String.format("onActionItemClicked item=%s/%d", item.toString(), item.getItemId()));
+            CharacterStyle cs;
+            int start = et_answer_.getSelectionStart();
+            int end = et_answer_.getSelectionEnd();
+            if (start == -1 || end == -1) {
+                return false;
+            }
+            SpannableStringBuilder ssb = new SpannableStringBuilder(et_answer_.getText());
+            for(StyleSpan s : ssb.getSpans(start, end, StyleSpan.class))
+            {
+                ssb.removeSpan(s);
+            }
+
+            switch (item.getItemId()) {
+
+                case R.id.bold:
+                    cs = new StyleSpan(Typeface.BOLD);
+                    ssb.setSpan(cs, start, end, 1);
+                    et_answer_.setText(ssb);
+                    return true;
+
+                case R.id.italic:
+                    cs = new StyleSpan(Typeface.ITALIC);
+                    ssb.setSpan(cs, start, end, 1);
+                    et_answer_.setText(ssb);
+                    return true;
+
+                case R.id.underline:
+                    cs = new UnderlineSpan();
+                    ssb.setSpan(cs, start, end, 1);
+                    et_answer_.setText(ssb);
+                    return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(android.view.ActionMode mode) {
+
+        }
+    }
 }
